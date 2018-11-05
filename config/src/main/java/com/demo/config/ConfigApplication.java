7b54894215b2d713953ef823ab9394cedec77060
fandongbo <dg9259@163.com>
2018-11-05 22:43:12 +0800

    demo

--- config/src/main/java/com/demo/config/ConfigApplication.java
+++ config/src/main/java/com/demo/config/ConfigApplication.java
@@ -2,12 +2,12 @@ package com.demo.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
-@EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigApplication {
 
     public static void main(String[] args) {
--- first/firstservice/src/main/java/com/demo/firstservice/FirstserviceApplication.java
+++ first/firstservice/src/main/java/com/demo/firstservice/FirstserviceApplication.java
@@ -1,13 +1,16 @@
 package com.demo.firstservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableSwagger2
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class FirstserviceApplication {
 
     public static void main(String[] args) {
