4fdd1b4aaebbf6e5f0b2d62687a97e353a357fc7
fandongbo <fandongbo@bcpeople.com>
2018-11-06 17:15:55 +0800

    demo

--- api/src/main/java/com/demo/api/ApiApplication.java
+++ api/src/main/java/com/demo/api/ApiApplication.java
@@ -1,12 +1,46 @@
 package com.demo.api;
 
+import com.demo.common.FeignClientConfiguration;
+import com.demo.common.RestTemplateConfig;
+import io.swagger.annotations.Api;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableSwagger2
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@Import(value = {RestTemplateConfig.class, FeignClientConfiguration.class})
 public class ApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
+    }
+
+    @Bean
+    public Docket baseServiceApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+            .groupName("sevice-api")
+            .apiInfo(apiInfo())
+            .select()
+            .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+            .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+            .title("Service APIs")
+            .description("服务接口").version("1.0").build();
     }
 }
--- api/src/main/java/com/demo/api/TestController.java
+++ api/src/main/java/com/demo/api/TestController.java
@@ -1,10 +1,16 @@
 package com.demo.api;
 
+import com.demo.common.Employee;
+import com.demo.common.RestResponse;
 import com.demo.firstcommon.client.FirstClient;
 import com.demo.secondcommon.SecondClient;
-import com.netflix.discovery.converters.Auto;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -20,6 +26,46 @@ public class TestController {
     @Autowired
     private SecondClient secondClient;
 
+    @ApiOperation("firstServiceTest1")
+    @RequestMapping(value = "/firstservice/test1/", method = {RequestMethod.GET})
+    public RestResponse<String> firstServiceTest1() {
+        return firstClient.test1();
+    }
 
 
+    @ApiOperation("firstServiceTest2")
+    @RequestMapping(value = "/firstservice/test2/", method = {RequestMethod.GET})
+    public RestResponse<String> firstServiceTest2() {
+        return firstClient.test2();
+    }
+
+    @ApiOperation("firstServiceTest3")
+    @RequestMapping(value = "/firstservice/test3/{param}", method = {RequestMethod.GET})
+    public RestResponse<String> firstServiceTest3(@PathVariable(value = "param") String param) {
+        return firstClient.test3(param);
+    }
+
+    @ApiOperation("secondServiceTest1")
+    @RequestMapping(value = "/secondservice/test1/", method = {RequestMethod.GET})
+    public RestResponse<String> secondServiceTest1() {
+        return secondClient.test1();
+    }
+
+    @ApiOperation("secondServiceTest2")
+    @RequestMapping(value = "/secondservice/test2/", method = {RequestMethod.GET})
+    public RestResponse<String> secondServiceTest2() {
+        return secondClient.test2();
+    }
+
+    @ApiOperation("secondServiceTest3")
+    @RequestMapping(value = "/secondservice/test3/", method = {RequestMethod.GET})
+    public RestResponse<String> secondServiceTest3() {
+        return secondClient.test3();
+    }
+
+    @ApiOperation("secondServiceTest4")
+    @RequestMapping(value = "/secondservice/test4", method = {RequestMethod.POST})
+    public RestResponse<Employee> secondServiceTest4(@RequestBody Employee employee) {
+        return secondClient.test4(employee);
+    }
 }
